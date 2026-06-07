47ebf0c5567dbd93427f370e3b6a74131594c0c4
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-07 21:49:13 +0530

    problem solution update

--- pattern Printing/kite.java
+++ pattern Printing/kite.java
@@ -21,6 +21,9 @@ public class kite {
         //part 2
         for(int i=1;i<=r;i++){
             // spaces
+            if( i==1){
+                continue;
+            }
             for(int j=1;j<=i-1;j++){
                 System.out.print(" ");
             }
