2742e8c3e0b604c1edf1d2cf8cd161339083151a
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-08 14:41:12 +0530

    q1

---
+++ Methodexample/addition.java
@@ -0,0 +1,11 @@
+public class addition {
+    static void add(int a,int b){
+        int sum=a+b;
+        System.out.println("The sum is: " +sum);
+    }
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        add(num1,num2);
+    }
+}
