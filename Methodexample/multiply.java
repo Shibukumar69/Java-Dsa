ccb466709281129621a41d59d035acbadd01ad60
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-08 14:54:21 +0530

    q2

---
+++ Methodexample/multiply.java
@@ -0,0 +1,13 @@
+
+public class multiply {
+    
+    static void Multi(int a,int b){
+        int M=a*b;
+        System.out.println("The product is: " +M);
+    }
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        Multi(num1,num2);
+    }
+}
