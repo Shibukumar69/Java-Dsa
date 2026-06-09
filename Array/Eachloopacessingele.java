64d48f0fc9a34acf2cc0944360d9c0e2cbecb0e2
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-09 10:46:21 +0530

    user input

---
+++ Array/Eachloopacessingele.java
@@ -0,0 +1,8 @@
+public class Eachloopacessingele {
+    public static void main(String[] args) {
+        int arr[]={12,23,34,45,56};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Array/arrbyuser.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class arrbyuser {
+    public static void main(String[] args) {
+        int arr[];
+        arr=new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i :arr){
+            System.out.print(i + " ");
+        }
+       
+
+      
+    }
+}
