4e9a27359dd4b136ffd8ecc3c647115e4f7511fb
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-09 10:36:17 +0530

    array

---
+++ Array/asscessingele.java
@@ -0,0 +1,12 @@
+public class asscessingele {
+    public static void main(String[] args) {
+        int arr[]={12,23,34,45,56};
+        // arr=new int[5];
+
+        System.out.println("The first element is: " +arr[0]);
+        System.out.println("The second element is: " +arr[1]);
+        System.out.println("The third element is: " +arr[2]);
+        System.out.println("The fourth element is: " +arr[3]);
+        System.out.println("The fifth element is: " +arr[4]);
+    }
+}   
---
+++ Methodexample/returnmult.java
@@ -0,0 +1,17 @@
+public class returnmult {
+    
+
+    
+    static int Multi(int a,int b){
+        int M=a*b;
+        return M;
+    }
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int result=Multi(num1,num2);
+        System.out.println("The product is: " +result);
+    }
+
+
+}
