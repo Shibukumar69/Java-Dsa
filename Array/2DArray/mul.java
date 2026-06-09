dc4b62e40f37f7e049d5428343888916c4d4d9f9
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-09 12:26:29 +0530

    max

---
+++ Array/2DArray/mul.java
@@ -0,0 +1,13 @@
+
+public class mul {
+     public static void main(String[] args) {
+      int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int mul = 1;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                mul = mul * arr[i][j];
+            }
+        }
+        System.out.println("The product of the 2D array is: " + mul);
+    } 
+}
