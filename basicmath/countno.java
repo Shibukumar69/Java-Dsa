fe82dbe5fc407fa3839cb6e49b0a5b27956d221d
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-11 09:22:48 +0530

    sum of digit

---
+++ basicmath/countno.java
@@ -0,0 +1,14 @@
+
+public class countno {
+   public static void main(String[] args) {
+        int number = 12345;
+        int count = 0;
+
+        while (number != 0) {
+            number = number / 10;
+            count++;
+        }
+
+        System.out.println("The number of digits in the number is: " + count);
+    } 
+}
