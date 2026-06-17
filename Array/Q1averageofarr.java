f868ecd9ad2ed31fe9fc753c22f514512fb3bbbf
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-17 20:35:28 +0530

    q

--- Array/Q1averageofarr.java
+++ Array/Q1averageofarr.java
@@ -13,6 +13,7 @@
     for(int i=0;i<n;i++){
       sum=sum+arr[i];
     }
-    System.out.println("The average of the array is: "+(sum/n));
+    double average=(double)sum/n;
+    System.out.println("The average of the array is: "+average);
   }
 }
