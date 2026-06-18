21fefd082120025c4fe919071b20a11458d0e0c7
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-18 11:37:32 +0530

    swaping alternate element of array

---
+++ Array/swapalter.java
@@ -0,0 +1,14 @@
+public class swapalter {
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5,6};
+        for(int i=0;i<arr.length-1;i+=2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+        System.out.println("The array after swapping alternate elements is: ");
+        for(int i:arr){ 
+            System.out.print(i + " ");
+        }
+    }
+}
