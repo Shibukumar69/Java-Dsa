eb9600cbe262587fdcf3a9edeb18ec46ba2c0b4f
Shibukumar69 <shibukumar3935@gmail.com>
2026-06-18 15:05:55 +0530

    mapping methods

--- JavaCollection framework/mapping.java
+++ JavaCollection framework/mapping.java
@@ -7,5 +7,14 @@ public class mapping {
         mapping.put("USA",2);
         mapping.put("UK",3);
         System.out.println("The mapping is: " + mapping);
+        Map<String, Integer> mapping2 = new HashMap<>();
+        mapping2.put("Russia",1);
+        mapping2.put("China",2);
+        mapping2.put("Japan",3);
+        System.out.println("The second mapping is: " + mapping2);
+        mapping.putAll(mapping2);
+        System.out.println("The mapping after adding the second mapping is: " + mapping);
+        mapping.remove("USA");
+        System.out.println("The mapping after removing USA is: " + mapping);
     }
 }
