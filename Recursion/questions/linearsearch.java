64bbcf43cf77ab70df63fe64545a121c521ae33c
Shibu kumar <shibukumar3935@gmail.com>
2026-08-01 12:09:45 +0530

    minimun in arrray and libnear search

---
+++ Recursion/questions/linearsearch.java
@@ -0,0 +1,12 @@
+public class linearsearch {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        int target=4;
+        System.out.println(search(arr,target,0));
+    }
+     static int search(int arr[],int target,int i){
+        if(i>=arr.length) return -1;
+        if(arr[i]==target) return i;
+        return search(arr,target,i+1);
+}
+}
--- Recursion/questions/max.java
+++ Recursion/questions/max.java
@@ -3,9 +3,12 @@ public class max {
         int arr1[] = {1,2,3,4,64,56,33,88,35};
 
         int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         int maxvalue = findmax(arr1, 0, max);
+        int minvalue = findmin(arr1, 0, min);
         System.out.println(maxvalue);
+        System.out.println(minvalue);
     }
 
     public static int findmax(int arr[], int i, int max) {
@@ -19,5 +22,17 @@ public class max {
         }
 
         return findmax(arr, i + 1, max);
+    }
+    public static int findmin(int arr[], int i, int min) {
+
+        if (i >= arr.length) {
+            return min;
+        }
+
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        return findmin(arr, i + 1, min);
     }
 }
